98d54e421eb034349cfed0161cdb6066e9c9d737
823770059@qq.com <823770059@qq.com>
2020-07-07 14:43:42 +0800

    second commit

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,22 @@
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = {
   "transpileDependencies": [
     "vuetify",
   ],
   configureWebpack: {
+     plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ],
     externals: {
       'vue': 'Vue', // 配置使用CDN
       'vue-router': 'VueRouter', // 配置使用CDN
