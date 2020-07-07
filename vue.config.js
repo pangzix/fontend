04b8a9027a34866bf38a780dee47d4cf9592bc82
823770059@qq.com <823770059@qq.com>
2020-07-07 14:18:42 +0800

    second commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify",
-  ]
+  ],
+  configureWebpack: {
+    externals: {
+      'vue': 'Vue', // 配置使用CDN
+      'vue-router': 'VueRouter', // 配置使用CDN
+      'vuetify': 'vuetify', // 配置使用CDN
+      'axios': 'axios', // 配置使用CDN
+    }
+  }
 }
 
